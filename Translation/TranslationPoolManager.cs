6b99eaafb23e9f0e7d9bc1f2da86165ae3426a96
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-04-18 20:15:10 +0100

    Create TranslationPoolManager.cs

---
+++ Translation/TranslationPoolManager.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ReachForStars.Translation
+{
+  public static class TransManager
+  {
+    public static string GetTranslatedText(TranslationPool pool)
+    {
+      //get the AU base Lang and return the string via switch case
+    }
+  }
+}
