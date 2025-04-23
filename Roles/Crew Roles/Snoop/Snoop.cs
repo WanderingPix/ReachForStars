d186050ca6439c5f1b27f2cc76008be372a7e9a5
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-04-23 17:43:06 +0100

    Snoop Role

---
+++ Roles/Crew Roles/Snoop/Snoop.cs
@@ -0,0 +1,27 @@
+﻿using MiraAPI.Roles;
+using MiraAPI.Utilities.Assets;
+using ReachForStars.Translation;
+using UnityEngine;
+
+namespace ReachForStars.Roles.Crewmates.Snoop;
+
+public class SnoopRole : CrewmateRole, ICustomRole
+{
+    public string RoleName => Rolename.GetTranslatedText(); 
+    public TranslationPool Rolename = new TranslationPool(
+        english: "Snoop",
+        spanish: "Sheriff",//TODO these translations
+        portuguese: "Xerife",
+        french: "Shérif"
+    );
+    public string RoleLongDescription => "Choose who to shoot";
+    public string RoleDescription => RoleLongDescription;
+    public Color RoleColor => Palette.White;
+    public ModdedRoleTeams Team => ModdedRoleTeams.Crewmate;
+
+    public Color OptionsMenuColor => Palette.CrewmateBlue;
+
+    public CustomRoleConfiguration Configuration => new CustomRoleConfiguration(this)
+    {
+    };
+}
