8ccd86e2832c3a3dcb43da32751b7b99b709d135
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-04-23 17:59:09 +0100

    Partial Snoop translations

--- Roles/Crew Roles/Snoop/Snoop.cs
+++ Roles/Crew Roles/Snoop/Snoop.cs
@@ -10,11 +10,11 @@ public class SnoopRole : CrewmateRole, ICustomRole
     public string RoleName => Rolename.GetTranslatedText(); 
     public TranslationPool Rolename = new TranslationPool(
         english: "Snoop",
-        spanish: "Sheriff",//TODO these translations
-        portuguese: "Xerife",
-        french: "Shérif"
+        spanish: "fisgón"
+        portuguese: "bisbilhoteira",
+        french: "Espion"
     );
-    public string RoleLongDescription => "Choose who to shoot";
+    public string RoleLongDescription => "Gather Info on the crew"; //TODO trans
     public string RoleDescription => RoleLongDescription;
     public Color RoleColor => Palette.White;
     public ModdedRoleTeams Team => ModdedRoleTeams.Crewmate;
