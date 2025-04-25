646658b693e1be46cb29b1a1bff45c01fba792ab
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-04-25 23:08:01 +0100

    Jester options

---
+++ Roles/Neutral Roles/Jester/JesterOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using MiraAPI.GameOptions;
+using MiraAPI.GameOptions.Attributes;
+
+namespace ReachForStars.Roles.Neutrals.Jester
+{
+    public class JesterOptions : AbstractOptionGroup<JesterRole>
+    {
+        public override string GroupName => "Custom Role";
+
+        [ModdedToggleOption("Jester Can Call Meeting")]
+        public bool CanCallMeeting { get; set; } = false;
+}
