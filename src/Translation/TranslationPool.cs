9a9e64417af30050dfd1028f1855e42b4ce6897e
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-05-01 21:33:24 +0100

    Update TranslationPool.cs

--- src/Translation/TranslationPool.cs
+++ src/Translation/TranslationPool.cs
@@ -21,37 +21,5 @@ namespace ReachForStars.Translation
        Russian = russian;
        Italian = italian;
      }
-        
-       ///Could I use a switch case here?
-       ///Yes.
-       ///Will I?
-       ///No.
-       ///Why?
-       ///Because I'm a lazy cunt
-       ///I live in your walls  ^_^  -Pengun
-       public static string GetTranslatedText(this TranslationPool pool)
-       {
-       if (TranslationController.Instance.currentLanguage.languageID == SupportedLangs.Spanish)
-         {
-         return pool.Spanish; //Spanish translator: TBD
-         }
-         else if (TranslationController.Instance.currentLanguage.languageID == SupportedLangs.Russian && pool.Russian != null)
-         {
-         return pool.Russian; //Russian translator: lime
-         }
-         else if (TranslationController.Instance.currentLanguage.languageID == SupportedLangs.Portuguese && pool.Portuguese != null)
-         {
-           return pool.Portuguese; //Portuguese translator: TBD
-         }
-         else if (TranslationController.Instance.currentLanguage.languageID == SupportedLangs.French && pool.French != null)
-         {
-           return pool.French; //french translator: WanderingPix
-         }
-         else if (TranslationController.Instance.currentLanguage.languageID == SupportedLangs.Italian && pool.Italian != null)
-         {
-           return pool.Italian; //french translator: pengun
-         }
-         else return pool.English;
-      }
     }
   }
