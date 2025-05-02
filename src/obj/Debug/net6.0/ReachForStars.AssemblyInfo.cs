afb1d77b0c9714f1bf12db968746c32ca558d4a1
WanderingPixel <126269533+shortbad@users.noreply.github.com>
2025-05-02 17:34:26 +0200

    sync

--- src/obj/Debug/net6.0/ReachForStars.AssemblyInfo.cs
+++ src/obj/Debug/net6.0/ReachForStars.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("The Silliest Among Us Mod")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a781138f0ba53ae4fdb24d84ab1b7843fdcc45c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b367db88f48c4cfb530f0e3796da5afe14174a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReachForStars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReachForStars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
