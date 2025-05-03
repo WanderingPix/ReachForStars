47d74f653894d5af93f679fe0844db870896f218
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-05-03 22:46:52 +0100

    Create AdminButton.cs

---
+++ src/Roles/Crew Roles/Snoop/AdminButton.cs
@@ -0,0 +1,39 @@
+using MiraAPI.GameOptions;
+using MiraAPI.Hud;
+using MiraAPI.Utilities.Assets;
+using Reactor.Utilities;
+using System.Collections;
+using UnityEngine;
+using MiraAPI.Utilities;
+using MiraAPI.Networking;
+using TMPro;
+using ReachForStars.Translation;
+using Reactor.Utilities.Extensions;
+
+namespace ReachForStars.Roles.Crewmates.Snoop;
+public class AdminButton : CustomActionButton
+{
+    public override string Name => ButtonName.GetTranslatedText();
+
+    public TranslationPool ButtonName = new TranslationPool(
+        english: "View",
+        spanish: "TBD",
+        portuguese: "TBD",
+        french: "TBD"
+    );
+    public override float Cooldown => 0;
+    public override float EffectDuration => 0;
+
+    public override int MaxUses => 0;
+
+    public override LoadableAsset<Sprite> Sprite => Assets.Shoot;
+
+    public override bool Enabled(RoleBehaviour? role)
+    {
+        return role is SnoopRole;
+    }
+    protected override void OnClick()
+    {
+        MapBehaviour.Instance.ShowAdminMap();
+    }
+}
