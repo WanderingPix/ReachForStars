2c660ca1bbd2f7132ec2996a83fa8b2498a0f360
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-05-03 23:14:40 +0100

    Update AdminButton.cs

--- src/Roles/Crew Roles/Snoop/AdminButton.cs
+++ src/Roles/Crew Roles/Snoop/AdminButton.cs
@@ -34,6 +34,6 @@ public class AdminButton : CustomActionButton
     }
     protected override void OnClick()
     {
-        MapBehaviour.Instance.ShowNormalMap()
+        MapBehaviour.Instance.ShowSabotageMap();
     }
 }
