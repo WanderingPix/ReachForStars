69a583c411f26c4323d9a22da7239927fdafacc6
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-05-04 17:20:33 +0100

    Update AdminButton.cs

--- src/Roles/Crew Roles/Snoop/AdminButton.cs
+++ src/Roles/Crew Roles/Snoop/AdminButton.cs
@@ -20,7 +20,7 @@ public class AdminButton : CustomActionButton
         spanish: "TBD",
         portuguese: "TBD",
         french: "Administration",
-        russian: "TBD"
+        russian: "TBD",
         italian: "TBD"
     );
     public override float Cooldown => 0;
