8f36419ee3502d2b01d7ca0c745151a55ef34824
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-05-07 15:25:44 +0100

    Update RPCs.cs

--- src/Networking/RPCs.cs
+++ src/Networking/RPCs.cs
@@ -2,16 +2,12 @@ namespace ReachForStars.Networking
 {
     public enum RPC : uint
     {
-        DeathReasons,
-        SetColor,
-        RoleBlock,
         Yeehaw,
         SeekerScream,
         ChangeBodyType,
         PlaceDaVent,
         ResizePlayer,
         DestroyObj,
-        Rampage,
         Revive,
         FreezeBody,
         DamageFrozenBody,
