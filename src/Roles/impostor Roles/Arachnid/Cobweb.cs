81a62b523f4283d95a1a40218f6a4db55f9f47ef
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-05-26 21:24:32 +0100

    Update Cobweb.cs

--- src/Roles/impostor Roles/Arachnid/Cobweb.cs
+++ src/Roles/impostor Roles/Arachnid/Cobweb.cs
@@ -12,17 +12,14 @@ using System.Linq;
 
 namespace ReachForStars.Roles.Impostors.Arachnid;
 
-public class FrozenBody : MonoBehaviour(ptr)
+public class Cobweb : MonoBehaviour(ptr)
 {
-    public BoxCollider2D myCol;
     public SpriteRenderer myRend;
   
     public void Start()
     {
         //Setup
         gameObject.transform.localScale = new Vector3(0.35f, 0.35f, 0.45f);
-        myCol = gameObject.AddComponent<BoxCollider2D>();
-        myCol.size = gameObject.transform.localScale;
         myRend = gameObject.AddComponent<SpriteRenderer>();
         gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, -5f);
         myRend.sprite = Assets.FrozenBody0.LoadAsset();
@@ -31,11 +28,11 @@ public class FrozenBody : MonoBehaviour(ptr)
         HudManager.Instance.StartCoroutine(Effects.ColorFade(myRend, new Color(1f, 1f, 1f, 0f), new Color(1f, 1f, 1f, 1f), 0.4f));
         HudManager.Instance.StartCoroutine(Effects.ScaleIn(gameObject.transform, 0f, 0.5f, 0.4f));
     }
-    public void OnCollisionEnter2D(Collider2D col)
+    public void FixedUpdate()
     {
-        if (col.gameObject.GetComponent<PlayerControl>())
+        foreach (PlayerControl p in Helpers.GetAlivePlayers().Where(x => Vector3.Distance(gameObject.transform.position, p.GetTruePosition()) > 1f))
         {
-            //TBD
+            p.AddModifier<SlowedDownModifier>();
         }
     }
 }
