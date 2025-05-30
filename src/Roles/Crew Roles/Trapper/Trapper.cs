df754f105a5bae19f2a03971849e7c83fd7b8d5d
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-05-30 23:22:28 +0100

    Create Trapper.cs

---
+++ src/Roles/Crew Roles/Trapper/Trapper.cs
@@ -0,0 +1,21 @@
+﻿using MiraAPI.Roles;
+using TMPro;
+using UnityEngine;
+using ReachForStars.Translation;
+
+namespace ReachForStars.Roles.Crewmates.Trapper;
+
+
+public class TrapperRole : CrewmateRole, ICustomRole
+{
+    public string RoleName => "Trapper";
+    public string RoleLongDescription => "ja ja ja you have not win";
+    public string RoleDescription => RoleLongDescription;
+    public Color RoleColor => Palette.White;
+    public Color OptionsMenuColor => Palette.CrewmateBlue;
+    public ModdedRoleTeams Team => ModdedRoleTeams.Crewmate;
+    public CustomRoleConfiguration Configuration => new CustomRoleConfiguration(this)
+    {
+        //OptionsScreenshot = ExampleAssets.Banner,
+    }; 
+}
