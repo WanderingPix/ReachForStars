b41b7830cd79864422bd186970a310c36a908726
WanderingPixel <126269533+shortbad@users.noreply.github.com>
2025-06-03 23:27:54 +0200

    slight changes bts

--- src/Roles/impostor Roles/Arachnid/SlowedDownModifier.cs
+++ src/Roles/impostor Roles/Arachnid/SlowedDownModifier.cs
@@ -5,9 +5,9 @@ using MiraAPI.Modifiers;
 
 namespace ReachForStars.Roles.Impostors.Arachnid
 {
-    public class SlowedDownModifier : TimedModifier
+    public class SlowedDownModifier : GameModifier
     {
-        public void Update()
+        public override void FixedUpdate()
         {
             Player.MyPhysics.body.velocity *= new Vector2(0.4f, 0.2f);
             if (Player.GetNearestObjectOfType<Cobweb>(1f, new ContactFilter2D().NoFilter()) != null)
@@ -15,7 +15,14 @@ namespace ReachForStars.Roles.Impostors.Arachnid
                 Player.RemoveModifier<SlowedDownModifier>();
             }
         }
-        public override float Duration => 1f;
+        public override int GetAmountPerGame()
+        {
+            return 0;
+        }
+        public override int GetAssignmentChance()
+        {
+            return 0;
+        }
         public override string ModifierName => "Slowed Down";
 
         public override string GetHudString()
