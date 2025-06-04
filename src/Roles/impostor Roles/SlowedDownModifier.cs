30343734c7a17f65d1b85bb4dbdbf970f71da6f9
WanderingPixel <126269533+shortbad@users.noreply.github.com>
2025-06-04 14:56:28 +0200

    we good?

---
+++ src/Roles/impostor Roles/SlowedDownModifier.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using MiraAPI.Modifiers.Types;
+using MiraAPI.Utilities;
+using MiraAPI.Modifiers;
+
+namespace ReachForStars.Roles.Impostors.Arachnid
+{
+    public class SlowedDownModifier : GameModifier
+    {
+
+        public override string ModifierName => "Slowed Down";
+
+        public override string GetHudString()
+        {
+            return "You are slowed down!";
+        }
+
+        public override bool ShowInFreeplay => false;
+        public override void FixedUpdate()
+        {
+            Player.MyPhysics.body.velocity *= new Vector2(0.4f, 0.2f);
+            if (Player.GetNearestObjectOfType<Cobweb>(1f, new ContactFilter2D().NoFilter()) != null)
+            {
+                Player.RemoveModifier<SlowedDownModifier>();
+            }
+        }
+
+        public override int GetAssignmentChance()
+        {
+            return 0;
+        }
+
+        public override int GetAmountPerGame()
+        {
+            return 0;
+        }
+    }
+}
--- src/obj/Release/net6.0/ReachForStars.AssemblyInfo.cs
+++ src/obj/Release/net6.0/ReachForStars.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("The Silliest Among Us Mod")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f5f0c53574ced29469b4f34574f3946da0d4d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26efeb54f98528ce21a981e87e4b6cb2d58c750a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReachForStars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReachForStars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
