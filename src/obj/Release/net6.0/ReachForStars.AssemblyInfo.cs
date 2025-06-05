5525a7a05bcfef5457eea6097e2e458041d206b5
WanderingPixel <126269533+shortbad@users.noreply.github.com>
2025-06-05 15:48:02 +0200

    one more thing

--- src/obj/Release/net6.0/ReachForStars.AssemblyInfo.cs
+++ src/obj/Release/net6.0/ReachForStars.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("The Silliest Among Us Mod")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30edcc34db84ae2bc473ff71399c42454f4693bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b81aa982b7e8a7632abf1b8438d2721b8cc69a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReachForStars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReachForStars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
