a25c0c7445e6491b0c582f402747224801965755
WanderingPixel <126269533+shortbad@users.noreply.github.com>
2025-06-15 16:07:02 +0100

    oops

--- src/RFSAssets.cs
+++ src/RFSAssets.cs
@@ -29,7 +29,7 @@ public static class Assets
     //Trapper
     public static LoadableResourceAsset PlaceTrapButton { get; } = new("ReachForStars.Resources.PlaceTrap.png");
     public static LoadableAudioResourceAsset TrapCloseSfx { get; } = new("ReachForStars.Resources.SoundEffects.TrapClose.wav");
-    public static LoadableAudioResourceAsset TrapPlaceSfx { get; } = new("ReachForStars.Resources.SoundEffects.TrapPlace.wav");
+    public static LoadableAudioResourceAsset TrapPlaceSfx { get; } = new("ReachForStars.Resources.SoundEffects.PlaceTrap.wav");
     public static LoadableBundleAsset<GameObject> StunnedPrefab { get; } = new("StunnedPrefab.prefab", Bundle);
     public static LoadableBundleAsset<GameObject> TrapPrefab { get; } = new("TrapPrefab.prefab", Bundle);
     public static LoadableBundleAsset<RuntimeAnimatorController> TrapCloseAnimationController { get; } = new("TrapPrefab.controller", Bundle);
