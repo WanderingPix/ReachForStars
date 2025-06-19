8acfc83c38b8f5252e60dd6d0e7f8c68aff5baf0
Matveydo3456 <164864997+Matveydo3456@users.noreply.github.com>
2025-06-19 15:10:20 +0300

    Update NeutralWinner.cs

--- src/Roles/Neutrals/NeutralWinner.cs
+++ src/Roles/Neutrals/NeutralWinner.cs
@@ -27,7 +27,7 @@ namespace ReachForStars.Roles.Neutrals
             english: "You have won! sit back watch the rest of the game unfold!",
             french: "Vous avez gagner! Regardez le reste de la partie!",
             spanish: "¡Has ganado! ¡Siéntate y observa cómo se desarrolla el resto del juego!",
-            russian: "",
+            russian: "Вы выиграли! расслабьтесь и наблюдайте за ходом игры!",
             italian: "Hai vinto! Divertiti a guardare la partita!"
         );
         public override void OnActivate()
