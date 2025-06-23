2ec39af3386eb8c577e88a9a2f49436b0ee983ca
WanderingPixel <126269533+shortbad@users.noreply.github.com>
2025-06-23 20:56:15 +0100

    Fix messed up coloring for RecolorableAbilityCounter

--- src/obj/Release/net6.0/ReachForStars.AssemblyInfo.cs
+++ src/obj/Release/net6.0/ReachForStars.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("The Silliest Among Us Mod")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77020856ae8bfaeabda321ba83d8ad786e45aadf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+584fb44cbca06231f5baa60cae94fe4b4269ebaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReachForStars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReachForStars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
